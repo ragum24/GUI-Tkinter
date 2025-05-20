90cc90291c42dbfbe2f0ea98b79cf1061d4c85bd
ragum24 <165797568+ragum24@users.noreply.github.com>
2025-05-20 19:17:58 +1000

    Add files via upload

--- guess_da_num.py
+++ guess_da_num.py
@@ -8,19 +8,42 @@ grooooooot.title("Guess The Number Game")
 
 grooooooot.geometry("400x100")
 
+secret_num=random.randint(1,20)
+
+count=0
+
+def OK():
+    name=entry.get()
+    messagebox.showinfo(message=f"Hey {name}!\nTake a Guess: 1-20")
+
+def Guess():
+    global count
+    name=entry.get()
+    guess=int(entry1.get())
+    if guess==secret_num:
+        messagebox.showinfo(message=f"{name}, you are correct!\n You took...{count} tries!👌👍😊")
+    elif guess>secret_num:
+        messagebox.showinfo(message=f"Your guess, {guess} is too high...😒")
+        count+=1
+    elif guess<secret_num:
+        messagebox.showinfo(message=f"Your guess, {guess} is too low...😒😂🤣")
+        count+=1
+    
+       
+
 label=Label(grooooooot,text="Welcome to our game!!!",font=("Times New Roman",15,"normal"))
 label.grid(row=0,column=1)
 
 label1=Label(grooooooot,text="Siapa nama kamu?")
 label1.grid(row=1,column=0)
 
-button=Button(grooooooot,text="OK")
+button=Button(grooooooot,text="OK",command=OK)
 button.grid(row=1,column=2)
 
 label2=Label(grooooooot,text="Take a guess-")
 label2.grid(row=2,column=0)
 
-button1=Button(grooooooot,text="Guess")
+button1=Button(grooooooot,text="Guess",command=Guess)
 button1.grid(row=2,column=2)
 
 entry=Entry(grooooooot)
