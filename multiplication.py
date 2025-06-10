55766963722fbf49dd35236ddcd92e1c7a46e389
ragum24 <165797568+ragum24@users.noreply.github.com>
2025-06-10 19:32:10 +1000

    Add files via upload

--- multiplication.py
+++ multiplication.py
@@ -7,7 +7,14 @@ grooooot_4_ever.title("Multiplication")
 
 grooooot_4_ever.geometry("200x300")
 
-button=Button(grooooot_4_ever,text="Generate",bd=10,background="light grey",foreground="black",font=("Calibri",10))
+def something():
+    table=""
+    for i in range(rad_num.get()):
+        table+=str(num.get())+"x"+ str(i)+"="+ str(num.get()*i)+"\n"
+    label3.config(text=table)
+
+
+button=Button(grooooot_4_ever,text="Generate",bd=10,background="light grey",foreground="black",font=("Calibri",10),command=something)
 button.grid(row=3,column=0,columnspan=3)
 
 num=IntVar()
@@ -19,6 +26,10 @@ label1.grid(row=0,column=0,columnspan=3,pady=20)
 
 label2=Label(grooooot_4_ever,text="Number:")
 label2.grid(row=1,column=0,padx=10,pady=25)
+
+label3=Label(grooooot_4_ever,text="")
+label3.grid(row=4,column=0)
+
 
 from tkinter.ttk import *
 
@@ -34,6 +45,8 @@ radio2.grid(row=2,column=1,padx=10)
 
 radio3=Radiobutton(grooooot_4_ever,text="30",variable=rad_num,value=30)
 radio3.grid(row=2,column=2,pady=10,padx=15)
+
+
 
 import tkinter as tk
  
