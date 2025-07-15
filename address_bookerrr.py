612652d6709abb22bb471aa4ad8baa3eeeac4b6e
ragum24 <165797568+ragum24@users.noreply.github.com>
2025-07-15 19:28:44 +1000

    Add files via upload

--- address_bookerrr.py
+++ address_bookerrr.py
@@ -1,10 +1,29 @@
 from tkinter import *
 
+from tkinter.filedialog import *
+
 from tkinter import messagebox
 
 groots=Tk()
 
 infooooh={}
+
+def small_window(event):
+    global grooooot_junior,select,details,key,values
+    grooooot_junior=Toplevel(groots)
+    select=lllliiiiisssstttt.curselection()
+    details=""
+    if select:
+        key=lllliiiiisssstttt.get(select)
+        values=infooooh[key]
+        details="Name:"+key+"\n"
+        details+="Adress:"+values[0]+"\n"
+        details+="Mobile:"+values[1]+"\n"
+        details+="Email:"+values[2]+"\n"
+        details+="Birthday"+values[3]+"\n"
+    Lb=Label(grooooot_junior,text=details)
+    Lb.pack()
+
 
 def clear_everythinggggggg():
     entry1.delete(0,END)
@@ -37,14 +56,44 @@ def edit():
         messagebox.showinfo("ERROR!!!","Please add your name first!")
 
 
+def delete():
+    selectt=lllliiiiisssstttt.curselection()
+    if selectt:
+        del infooooh[lllliiiiisssstttt.get(selectt)]
+        lllliiiiisssstttt.delete(selectt)
+    else:
+        messagebox.showinfo("ERROR!","Please make a selection first!")
+
+def saveee():
+    file=asksaveasfile(defaultextension=".txt")
+    if file:
+        print(infooooh,file=file)
+        lllliiiiisssstttt.delete(0,END)
+        infooooh.clear()
+    else:
+        messagebox.showinfo("ERROR!","The file has not been saved.")
+
+def open_file():
+    global infooooh
+    lllliiiiisssstttt.delete(0,END)
+    infooooh.clear()
+    file=askopenfile()
+    if file:
+        infooooh=eval(file.read())
+        for i in infooooh:
+            lllliiiiisssstttt.insert(END,i) 
+    else:
+        messagebox.showinfo("ERROR!","No file has been selected.")  
+
 label=Label(groots,text="My Address Book")
 label.grid(row=0,column=1,columnspan=3)
 
-open_b=Button(groots,text="Open",bg="white")
+open_b=Button(groots,text="Open",bg="white",command=open_file)
 open_b.grid(row=0,column=4)
 
 lllliiiiisssstttt=Listbox(groots,height=15,width=30)
 lllliiiiisssstttt.grid(row=1,column=0,columnspan=3,rowspan=5)
+lllliiiiisssstttt.bind('<<ListboxSelect>>',small_window)
 
 name=Label(groots,text="Name:")
 name.grid(row=2,column=3)
@@ -82,9 +131,9 @@ update_b.grid(row=7,column=4,pady=30)
 edit_b=Button(groots,text="Edit",bg="white",command=edit)
 edit_b.grid(row=0,column=0)
 
-delete_b=Button(groots,text="Delete",bg="white")
+delete_b=Button(groots,text="Delete",bg="white",command=delete)
 delete_b.grid(row=7,column=0)
 
-save_b=Button(groots,text="Save!",width=35,bg="white")
+save_b=Button(groots,text="Save!",width=35,bg="white",command=saveee)
 save_b.grid(row=8,column=3)
 mainloop()
