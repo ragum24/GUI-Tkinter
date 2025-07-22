1e05a931b72b774c1b3499d34bcad91f1c6573ed
ragum24 <165797568+ragum24@users.noreply.github.com>
2025-07-22 19:13:42 +1000

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from gtts import gTTS 
+import os
+
+root=Tk()
+
+root.config()
+root.geometry("300x300")
+
+def text_2_speech():
+    gtttts=gTTS(text=entry.get(),lang="es",tld="es")
+    gtttts.save("AUDIOOH.wav")
+    os.system("AUDIOOH.wav")
+
+text2speech=Label(root,text="Text To Speech")
+text2speech.pack(pady=20)
+
+entry=Entry(root,width=30)
+entry.pack(pady=40)
+
+submit_b=Button(root,text="SUBMIT",fg="white",bg="dark orange",width=5,command=text_2_speech)
+submit_b.pack(pady=10)
+
+mainloop()
